a1d99ce2f316ff3b5b2c8d6eb1d5cd922b025b5b
tubaaNur <yesiltastubanur@gmail.com>
2023-09-08 00:27:32 +0300

    Forgot Password screen added

---
+++ NewsApp/ui/view/ForgotPasswordViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ForgatPasswordViewController.swift
+//  NewsApp
+//
+//  Created by Tuba Nur  on 8.09.2023.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+    }
+    
+
+}
